4438fc6ba93f4d221d74f15dd20733a3e22b7156
Yann Carvalho <yann@logusinfo.com.br>
2023-02-23 21:24:45 -0300

    feat: add integrated tests

--- auth/src/test/java/br/dev/yann/rssreader/auth/user/UserRepositoryTest.java
+++ auth/src/test/java/br/dev/yann/rssreader/auth/user/UserRepositoryTest.java
@@ -1,6 +1,11 @@
 package br.dev.yann.rssreader.auth.user;
 
+import static br.dev.yann.rssreader.auth.user.UserRole.ADMIN;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -18,21 +23,50 @@ class UserRepositoryTest {
 	   repository.deleteAll();
 	}
 	
-	@Test
-	void test(){
-		var userName = "user";
-		var userUsername = "user";
-		var userPassword = "user";
-		var userRoleUser = new User(userName, userUsername, userPassword);
-		repository.save(userRoleUser);
-		
-		var adminName = "admin";
-		var adminUsername = "admin";
-		var adminPassword = "admin";
-		var userRoleAdmin = new User(adminName, adminUsername, adminPassword);
-		userRoleAdmin.setRole(UserRole.ADMIN);
-		repository.save(userRoleAdmin);
-		
-
+	/**
+	 * Save an user in database
+	 * @param name user name
+	 * @param username user username
+	 * @param password user password
+	 * @param role user role
+	 * 
+	 * @see UserRole
+	 * 
+	 * @return the user save in the database
+	 */
+	private User saveUserDatabase(String name, String username, String password, UserRole role) {
+		var user = new User(name, username, password);
+		user.setRole(role);
+		return repository.saveAndFlush(user);
 	}
+	
+	@Test
+	@DisplayName("if user is the single admin then return true")
+	void ifUserTheSingleAdminThenReturnTrue() {
+		
+		//Arrange
+		var singleAdmin = saveUserDatabase("singleAdmin", "singleAdmin", "singleAdmin", ADMIN);
+		
+		//Act
+		var isSingleAdmin = repository.existsSoleUserAdminById(singleAdmin.getId());
+		
+		//Assert
+		assertTrue(isSingleAdmin);	
+	}
+	
+	@Test
+	@DisplayName("if not user is the single admin then return false")
+	void ifNotUserTheSingleAdminThenReturnFalse(){
+		
+		//Arrange
+		saveUserDatabase("firstAdmin", "firstAdmin", "firstAdmin", ADMIN);
+		var secondAdmin = saveUserDatabase("secondAdmin", "secondAdmin", "secondAdmin", ADMIN);
+		
+		//Act
+		var isSingleAdmin = repository.existsSoleUserAdminById(secondAdmin.getId());
+		
+		//Assert
+		assertFalse(isSingleAdmin);	
+	}
+	
 }
